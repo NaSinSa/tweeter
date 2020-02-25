5f649dcb46e26082e0d641c4387a8e5708be0c7b
NaSinSa <andy.k.oh@hotmail.com>
2020-02-25 18:41:10 -0500

    minor code style corrections

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('[name="text"]').on('keyup', function() {
     if ($(this).val().length > 140) {
-      $(this).siblings('.counter').css({color:'red'});
+      $(this).siblings('.counter').addClass('over140');
     } else {
-      $(this).siblings('.counter').css({color:'black'});
+      $(this).siblings('.counter').removeClass('over140');
     }
     $(this).siblings('.counter').html(140 - $(this).val().length);
   });
