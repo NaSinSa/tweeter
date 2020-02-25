d7429398647fa59a267477a20144158b5496fc30
NaSinSa <andy.k.oh@hotmail.com>
2020-02-25 15:12:53 -0500

    over 140 char, counter color changes to red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  const tweetInput = '[name="text"]';
-  $(tweetInput).on('keyup', function() {
+  $('[name="text"]').on('keyup', function() {
     if ($(this).val().length > 140) {
-      $(this).val($(this).val().substring(0, 140));
+      $(this).siblings('.counter').css({color:'red'});
+    } else {
+      $(this).siblings('.counter').css({color:'black'});
     }
     $(this).siblings('.counter').html(140 - $(this).val().length);
   });
