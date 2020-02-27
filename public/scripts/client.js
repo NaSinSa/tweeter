434641b3c61ff1766b82f7e66e5f3ed36a976b8f
NaSinSa <andy.k.oh@hotmail.com>
2020-02-27 15:48:26 -0500

    now the app is responsive. In mobile version, my avatar is smaller, nav background is hidden. I Desktop version my avatar is on left side. Day calculator is updated. It wil show seconds to days

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,15 +5,30 @@
  */
 
 
-const whenTheTweetIsPosted = function(nb) {
-  let days = Math.ceil((new Date() - nb) / 1000 / 60 / 60 / 24);
-  return `${days} days ago`
+const whenIsTheTweetPosted = function(nb) {
+  let inSecond = (new Date() - nb) / 1000;
+  if (inSecond < 60) {
+    return `${Math.round(inSecond)} second${timeUnitPluralChecker(Math.round(inSecond))}ago`;
+  } else if (inSecond / 60 < 60) {
+    return `${Math.round(inSecond / 60)} minute${timeUnitPluralChecker(Math.round(inSecond / 60))}ago`;
+  } else if (inSecond / 60 / 60 < 24) {
+    return `${Math.round(inSecond / 60 / 60)} hour${timeUnitPluralChecker(Math.round(inSecond / 60 / 60))}ago`;
+  } else {
+    return `${Math.round(inSecond / 60 / 60 / 24)} day${timeUnitPluralChecker(Math.round(inSecond / 60 / 60 / 24))}ago`;
+  }
+};
+
+const timeUnitPluralChecker = function(time) {
+  if (Math.round(time) > 1) {
+    return 's ';
+  } else {
+    return ' ';
+  }
 };
 
 const renderTweets = function(tweets) {
 
   for (let ele of tweets) {
-    // $('.tweet-container').append(createTweetElement(ele));
     createTweetElement(ele);
   }
 };
@@ -21,21 +36,7 @@ const renderTweets = function(tweets) {
 const createTweetElement = function(tweet) {
   let obj = tweet;
   let keys = Object.keys(tweet);
-  // let article = 
-  // `
-  // <article>
-  //   <header>
-  //     <h3><img src=${obj.user.avatars}> ${obj.user.name}</h3>
-  //     <span class='hide'>${obj.user.handle}</span>
-  //   </header>
-  
-  //   <p>${obj.content.text}</p>
-  
-  //   <footer>
-  //     <p>${whenTheTweetIsPosted(obj.created_at)}</p>
-  //     <button>flags</button>
-  //   </footer>
-  // </article>`;
+
   let $tweet = $('<article>');
   let $header = $('<header>');
   let $span = $('<span>').addClass('hide');
@@ -51,7 +52,7 @@ const createTweetElement = function(tweet) {
   $h3.text(obj.user.name);
   $header.append($h3.prepend($img));
   $header.append($span);
-  $footer.append($pTweet.text(whenTheTweetIsPosted(obj.created_at)));
+  $footer.append($pTweet.text(whenIsTheTweetPosted(obj.created_at)));
   $footer.append($button);
   
   $tweet
@@ -59,16 +60,15 @@ const createTweetElement = function(tweet) {
     .append($pDays.text(obj.content.text))
     .append($footer)
   
-    return $('.tweet-container').prepend($tweet)
-    // return article;
-  };
+  return $('.tweet-container').prepend($tweet)
+};
   
-  const loadTweets = function() {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-      success: function(data) {
-      renderTweets(data)
+const loadTweets = function() {
+  $.ajax({
+    method: 'GET',
+    url: '/tweets',
+    success: function(data) {
+      renderTweets(data);
     }
   });
 };
@@ -79,12 +79,12 @@ $(document).ready(function() {
     event.preventDefault();
     let tweet = $( this ).serialize();
     $('p[class="tweetErrorMsg"]').slideUp();
+
     if ($('textarea').val().length > 140) {
       $('p[name="tooLong"]').slideDown();
     } else if ($('textarea').val().length === 0) {
       $('p[name="noInput"]').slideToggle();
     } else {
-
       $.ajax({
         method: 'POST',
         url: '/tweets',
@@ -94,13 +94,13 @@ $(document).ready(function() {
         loadTweets(data);
         $('textarea').val("");
         $('.counter').text('140');
-      })
+      });
     }
   });
   
   $('nav div').on('click', function(event) {
     $('.tweeter').slideToggle(()=>{
-      $('textarea').focus()
+      $('textarea').focus();
     });
   })
 });
